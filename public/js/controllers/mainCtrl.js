27b0ce534a88ec734d241a266934cf3b0cad2c6b
Liuxizi Xu <xlxzbisulyn@gmail.com>
2015-09-01 21:21:11 -0400

    update content

--- public/js/controllers/mainCtrl.js
+++ public/js/controllers/mainCtrl.js
@@ -1,22 +1,15 @@
 angular.module('mainCtrl', []).controller('mainController', function($scope) {
 	$scope.logo = 'img/logo.png'
-	$scope.tagline = 'I sell stuff lol'
+	$scope.tagline = 'An Estate Sale in Mountain Brook'
 	$scope.sales = [
 		{
-			title: 'Estate Sale in Enlgish Village',
+			title: '',
 			satTime: 'Saturday: 10am-3pm',
 			sunTime: 'Sunday: 12pm-3pm',
-			address: '2930 Berwick Rd, Birmingham, AL 35213',
-			img1: 'img/home/barw-4.jpg',
-			img2: 'img/home/barw-14.jpg'
-		},
-		{
-			title: 'ONE DAY Estate Sale off Karl Daly Road in Birmingham',
-			satTime: 'Saturday: 10am-3pm ONLY',
-			sunTime: 'Sunday: N/A',
-			address: '1008 Bramble Ln, Irondale, AL 35210',
-			img1: 'img/home/bramble-1.jpg',
-			img2: 'img/home/bramble-7.jpg'
+			address: '3496 Westbury Rd, Birmingham, AL 35223',
+			img1: 'img/home/3.jpg',
+			img2: 'img/home/2.jpg',
+			img3: 'img/home/1.jpg'
 		}
 	]
 	$scope.startingTime = 'Numbers will be avaiable at 8am on Saturday'
